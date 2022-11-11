986fab36f0ade26c9a1ed390b8a13fc26802769d
Jackie Tsien <jackie10673311@gmail.com>
2022-11-11 11:53:03 +0800

    update: use typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,17 @@ import Layout, { siteTitle } from '../components/layout';
 import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
+import { GetStaticProps } from 'next'
 
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+export default function Home({
+  allPostsData
+}: {
+  allPostsData: {
+    date: string
+    title: string
+    id: string
+  }[]
+}) {
   return (
     <Layout home>
       <Head>
@@ -43,4 +43,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
